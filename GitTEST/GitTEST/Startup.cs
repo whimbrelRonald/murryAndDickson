456b0846203cd8748a7869f17be8cd84448be1da
Ronald <Ronald@whimbrel-software.com>
2015-06-10 13:33:44 +0200

    First Commit.

---
+++ GitTEST/GitTEST/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitTEST.Startup))]
+namespace GitTEST
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
